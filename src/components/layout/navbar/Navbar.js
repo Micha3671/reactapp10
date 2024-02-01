91c217eac3156294546ca4155cb22951c81fe35e
Micha <michael.rigas@t-online.de>
2024-02-01 11:49:08 +0100

    Newmain
    Kopie von reactapp5

---
+++ src/components/layout/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import styles from "./Navbar.module.css";
+import NavbarLeft from "./navbar-left/NavbarLeft";
+import NavbarRight from "./navbar-right/NavbarRight";
+
+function Navbar() {
+  return (
+    <div className={`${styles.mainContainer} border-bottom-shadow`}>
+      <NavbarLeft />
+      <div className={styles.spacer} />
+      <NavbarRight />
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/layout/navbar/navbar-right/NavbarRight.js
@@ -0,0 +1,15 @@
+import AppData from "../../../../utils/AppData";
+import StandardBtn from "../../../common/buttons/standard-btn/StandardBtn";
+import styles from "./NavbarRight.module.css";
+
+function NavbarRight() {
+  return (
+    <div className={styles.mainContainer}>
+      <StandardBtn text={"Login"} />
+      <div className={styles.spacer} />
+      <StandardBtn text={"Sign Up"} />
+    </div>
+  );
+}
+
+export default NavbarRight;
